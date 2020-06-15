afdc689ac97685bd03460445fad4e1a78cd619d5
Tahmid <47899219+TahmidMahin@users.noreply.github.com>
2020-06-15 14:54:03 +0600

    Update NeuralNetwork.java
    fuck this shit

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -7,6 +7,7 @@ public class NeuralNetwork {
 	private double[][] output;
 	private int trainingExample;
 	private HashMap<String, double[][]> parameters = new HashMap<String, double[][]>();
+	private HashMap<String, double[][]> cache = new HashMap<String, double[][]>();
 	private HashMap<String, double[][]> grads = new HashMap<String, double[][]>();
 	
 	public NeuralNetwork() {
@@ -45,7 +46,7 @@ public class NeuralNetwork {
 		return Numpy.subtract(1, Numpy.multiply(temp, temp));
 	}
 	
-	public double propagate() {
+	public double[][] forwardPropagation() {
 		double[][] W1 = parameters.get("W1");
 		double[][] b1 = parameters.get("b1");
 		double[][] W2 = parameters.get("W2");
@@ -60,8 +61,31 @@ public class NeuralNetwork {
 		double[][] Z3 = Numpy.add(Numpy.dot(W3, A2), b3);
 		double[][] A3 = Numpy.softmax(Z3);
 		
-		double cost = Numpy.sum(Numpy.multiply(output, Numpy.log(A3)));
+		cache.put("Z1", Z1);
+		cache.put("A1", A1);
+		cache.put("Z2", Z2);
+		cache.put("A2", A2);
+		cache.put("Z3", Z3);
+		cache.put("A3", A3);
+		
+		return A3;
+	}
+	
+	public double computeCost(double[][] yhat) {
+		double cost = Numpy.sum(Numpy.multiply(output, Numpy.log(yhat)));
 		cost /= -trainingExample;
+		return cost;
+	}
+	
+	public void backwardPropagation() {
+		double[][] W2 = parameters.get("W2");
+		double[][] W3 = parameters.get("W3");
+		
+		double[][] Z1 = cache.get("Z1");
+		double[][] A1 = cache.get("A1");
+		double[][] Z2 = cache.get("Z2");
+		double[][] A2 = cache.get("A2");
+		double[][] A3 = cache.get("A3");
 		
 		double[][] dZ3 = Numpy.subtract(A3, output);
 		double[][] dW3 = Numpy.multiply(Numpy.dot(dZ3, Numpy.transpose(A2)), 1/trainingExample);
@@ -79,7 +103,47 @@ public class NeuralNetwork {
 		grads.put("db2", db2);
 		grads.put("dW3", dW3);
 		grads.put("db3", db3);
-		
-		return cost;
 	}
+	
+	public void updateParameters(double learningRate) {
+		double[][] W1 = parameters.get("W1");
+		double[][] b1 = parameters.get("b1");
+		double[][] W2 = parameters.get("W2");
+		double[][] b2 = parameters.get("b2");
+		double[][] W3 = parameters.get("W3");
+		double[][] b3 = parameters.get("b3");
+		
+		double[][] dW1 = grads.get("dW1");
+		double[][] db1 = grads.get("db1");
+		double[][] dW2 = grads.get("dW2");
+		double[][] db2 = grads.get("db2");
+		double[][] dW3 = grads.get("dW3");
+		double[][] db3 = grads.get("db3");
+		
+		W1 = Numpy.subtract(W1, Numpy.multiply(learningRate, dW1));
+		b1 = Numpy.subtract(b1, Numpy.multiply(learningRate, db1));
+		W2 = Numpy.subtract(W1, Numpy.multiply(learningRate, dW2));
+		b2 = Numpy.subtract(b1, Numpy.multiply(learningRate, db2));
+		W3 = Numpy.subtract(W1, Numpy.multiply(learningRate, dW3));
+		b3 = Numpy.subtract(b1, Numpy.multiply(learningRate, db3));
+		
+		parameters.put("W1", W1);
+		parameters.put("b1", b1);
+		parameters.put("W2", W2);
+		parameters.put("b2", b2);
+		parameters.put("W3", W3);
+		parameters.put("b3", b3);
+	}
+	
+	public void gradientDescent(int iterations, double learningRate) {
+		for(int itr=0; itr<=iterations; itr++) {
+			double[][] yhat = forwardPropagation();
+			double cost = computeCost(yhat);
+			backwardPropagation();
+			updateParameters(learningRate);
+			if(itr%100 == 0)
+				System.out.println("cost after iteration " + itr + ": " +cost);
+		}
+	}
+	
 }
