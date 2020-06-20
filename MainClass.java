feab9101fb41f010aef1ffeafd522624612a48b2
enchanted-crow <56452731+enchanted-crow@users.noreply.github.com>
2020-06-20 08:43:33 +0600

    ideal configuration for max accuracy

--- MainClass.java
+++ MainClass.java
@@ -2,18 +2,19 @@
 public class MainClass {
     public static void main(String[] args) throws Exception {
         Dataset dt = new Dataset();
-        dt.initialize(42000, 9, 1);
-  
-	NeuralNetwork nn = new NeuralNetwork(dt.getTrainingDataArray(), dt.getTrainingExpectedResultsArray());
-	nn.gradientDescent(1000, 0.01);
-		
-		
+//        dt.initialize(10000, 1, 4);
+        dt.initialize(1800);
+
+        NeuralNetwork nn = new NeuralNetwork(dt.getTrainingDataArray(), dt.getTrainingExpectedResultsArray());
+        nn.gradientDescent(.001, 1);
+
 		////////////////////////////////////////////////////////////
 		// Enter your image directory here
 		// Example: test = {"parent_directory/Umaar.jpg"}
 		////////////////////////////////////////////////////////////
 		String[] test = {};
 		int[] result = nn.predict(dt.getTestDataArray());
+		System.out.println(Dataset.calculateTestAccuracy(result));
 		//for(int i=0;i<result.length;i++)
                     //System.out.println("");
     }
